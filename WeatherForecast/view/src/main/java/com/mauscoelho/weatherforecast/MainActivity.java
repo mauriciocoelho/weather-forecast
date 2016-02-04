d8102f2363d22f604da0ee57fc9e1d4907e23ce5
Mauricio Coelho <mauscoelho@gmail.com>
2016-02-03 23:36:57 -0200

    :art: retrive forecasts from internal storage

--- WeatherForecast/view/src/main/java/com/mauscoelho/weatherforecast/MainActivity.java
+++ WeatherForecast/view/src/main/java/com/mauscoelho/weatherforecast/MainActivity.java
@@ -3,19 +3,25 @@ package com.mauscoelho.weatherforecast;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.RecyclerView;
 import android.widget.Button;
 import android.widget.ImageView;
 
 import com.mauscoelho.controllers.services.InternalStorageService;
 import com.mauscoelho.data.CityForecast;
+import com.mauscoelho.weatherforecast.adapters.MainAdapter;
+
 import javax.inject.Inject;
 import butterknife.ButterKnife;
+import butterknife.InjectView;
 import butterknife.OnClick;
 
 
 public class MainActivity extends AppCompatActivity {
 
 
+    @InjectView(R.id.rv_forecast)
+    RecyclerView rv_forecast;
     @Inject
     InternalStorageService internalStorageService;
 
@@ -30,6 +36,10 @@ public class MainActivity extends AppCompatActivity {
 
     private void buildForecasts() {
         CityForecast[] cityForecasts = internalStorageService.getCities();
+        if(cityForecasts != null){
+            MainAdapter mainAdapter = new MainAdapter(cityForecasts);
+            rv_forecast.setAdapter(mainAdapter);
+        }
     }
 
 
